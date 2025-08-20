c45e158e8f1329e73d2729704bc30cb722ba59ba
brian-bfz <brianvanzhao@gmail.com>
2025-08-20 13:27:01 -0400

    bug fix

--- model/__init__.py
+++ model/__init__.py
@@ -1,6 +1,4 @@
-from .gnn import GNNDynamics
 from .transformer import TransformerDynamics
-from .gnn_dyn import PropNetDiffDenModel
 from .rollout import Rollout
 
 __all__ = ['GNNDynamics', 'TransformerDynamics', 'PropNetDiffDenModel', 'Rollout']
